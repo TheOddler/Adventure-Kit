eb67a90ee0edc9f4980e1b2d48006c4367f8fe8e
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-02-21 19:51:10 +0100

    Renamed Makes more sense, since this script is for little items you hand 'hold'.

--- Unity - Adventure Kit/Assets/Adventure Kit/Holdable.cs
+++ Unity - Adventure Kit/Assets/Adventure Kit/Holdable.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Dragable : MonoBehaviour,
+public class Holdable : MonoBehaviour,
     IBeginDragHandler,
     IDragHandler,
     IEndDragHandler
